667203b57741a7fff60878a522a555959a7f8237
Daria Mask <ferra@list.ru>
2024-02-03 17:29:01 +0300

    add entity unitType

---
+++ PriceCalculator/Areas/Identity/Data/UnitType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PriceCalculator.Areas.Identity.Data
+{
+    public class UnitType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }      
+        [NotMapped]public List<string> UnitTypes { get; set; } = new List<string>
+        {
+            "1 килограмм",
+            "1 литр",
+            "100 грамм",
+            "100 миллилитров",
+            "1 штука",
+        };
+    }
+}
